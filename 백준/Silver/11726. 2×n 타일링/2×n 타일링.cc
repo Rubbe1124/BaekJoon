b7936abde2bf9a8802a8a7635451e714997949b4
Rubbe1124 <61380448+Rubbe1124@users.noreply.github.com>
2023-08-31 17:11:03 +0900

    [Silver III] Title: 2×n 타일링, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.cc
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+
+using namespace std;
+
+int main()
+{
+	cin.tie(NULL);
+	cout.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	int arr[1001];
+	arr[1] = 1;
+	arr[2] = 2;
+
+	int n;
+	cin >> n;
+	for (int i = 3; i < n+1; i++)
+	{
+		arr[i] = arr[i - 1] + arr[i - 2];
+		arr[i] = arr[i] % 10007;
+	}
+
+	cout << arr[n]%10007 << "\n";
+}
